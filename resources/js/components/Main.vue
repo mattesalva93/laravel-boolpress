c207c3a09b0d1db6f7dedbfd292489b9635692a5
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-03-28 13:59:03 +0200

    home view with router-link done

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,80 +1,13 @@
 <template>
-    <!-- <main>
-        <div class="container">
-            <div class="post-container">
-                <div
-                    v-for="elemento in posts"
-                    :key="elemento.id"
-                    class="post-card"
-                >
-                    <div>TITOLAZZO: {{ elemento.title }}</div>
-                    <div>
-                        {{ elemento.content }}
-                    </div>
-                    <div v-if="elemento.image != null">
-                        <img :src="'storage/' + elemento.image" alt="" />
-                    </div>
-                    <div v-else>
-                        <img src="../../../public/img/empty_image.jpg" alt="" />
-                    </div>
-                    <div v-if="elemento.category != null">
-                        CATEGORIA: {{ elemento.category.name}}
-                    </div>
-                    <div v-else>
-                        CATEGORIA: -
-                    </div>
-                    <div>
-                        TAG: 
-                        <span v-for="(tag, index) in elemento.tags" :key="index">
-                            <i>{{tag.name}} </i>
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main> -->
-
     <router-link ></router-link>
 </template>
 
 <script>
 export default {
-    name: "Main",
-    data() {
-        return {
-            posts: [],
-        };
-    },
-    created() {
-        this.getPosts();
-    },
-    methods: {
-        getPosts: function () {
-            axios.get("/api/posts").then((response) => {
-                this.posts = response.data;
-            });
-        },
-    },
+    name: "Main"
+    
 };
 </script>
 
 <style lang="scss" scoped>
-.post-container {
-    margin-top: 100px;
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    .post-card {
-        border: 1px solid black;
-        width: 30%;
-        margin: 10px auto;
-        display: flex;
-        flex-direction: column;
-        text-align: center;
-        img {
-            height: 100px;
-            width: auto;
-        }
-    }
-}
 </style>
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,78 @@
+<template>
+      <main>
+        <div class="container">
+            <div class="post-container">
+                <div
+                    v-for="elemento in posts"
+                    :key="elemento.id"
+                    class="post-card"
+                >
+                    <div>TITOLAZZO: {{ elemento.title }}</div>
+                    <div>
+                        {{ elemento.content }}
+                    </div>
+                    <div v-if="elemento.image != null">
+                        <img :src="'storage/' + elemento.image" alt="" />
+                    </div>
+                    <div v-else>
+                        <img src="../../../public/img/empty_image.jpg" alt="" />
+                    </div>
+                    <div v-if="elemento.category != null">
+                        CATEGORIA: {{ elemento.category.name}}
+                    </div>
+                    <div v-else>
+                        CATEGORIA: -
+                    </div>
+                    <div>
+                        TAG: 
+                        <span v-for="(tag, index) in elemento.tags" :key="index">
+                            <i>{{tag.name}} </i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    data() {
+        return {
+            posts: [],
+        };
+    },
+    created() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts: function () {
+            axios.get("/api/posts").then((response) => {
+                this.posts = response.data;
+            });
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.post-container {
+    margin-top: 100px;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    .post-card {
+        border: 1px solid black;
+        width: 30%;
+        margin: 10px auto;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        img {
+            height: 100px;
+            width: auto;
+        }
+    }
+}
+</style>
